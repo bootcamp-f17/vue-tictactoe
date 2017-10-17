a98029d8b6100403e03960d48eb8b1c94faefe69
Janine Hempy <jhempy@gmail.com>
2017-10-17 11:53:30 -0400

    Xs and Os show properly on spaces

--- app/main.js
+++ app/main.js
@@ -1,11 +1,55 @@
 Vue.component('space', {
-
+  template: '<div v-on:click="callSquare()">{{ display }}</div>',
+  data: function() {
+    return {
+      display: '',
+    };
+  },
+  props: [ 'symbol' ],
+  methods: {
+    callSquare: function() {
+      if (this.display === '') {
+        // New square
+        this.display = this.symbol;
+        this.$emit('new');
+      }
+      else {
+        // I have been clicked already
+        this.$emit('repeat');
+      }
+    }
+  }
 });
 
 var app = new Vue({
 
-  el: '#app'  
+  el: '#app',
+  data: {
+    symbols: ['X', 'O'],
+    turn: 0
+  },
+  methods: {
+
+    newClick: function() {
+      console.log("clicked a new space");
+      this.turn++;
+      this.checkForWin();
+      this.checkForDraw();
+    },
+
+    repeatClick: function() {
+      console.log("clicked a space again");
+    },
+
+    checkForWin: function() {
+      alert("checking for win");
+    },
+
+    checkForDraw: function() {
+      alert("checking for draw");
+    }
 
 
+  }  
 
 });
