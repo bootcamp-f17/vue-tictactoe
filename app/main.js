b603077a3abbe9fe19aae48a760d241df6d07080
nickveil <31191433+nickveil@users.noreply.github.com>
2017-10-16 21:02:13 -0400

    InnerHTML issue

--- app/main.js
+++ app/main.js
@@ -1,71 +1,45 @@
-// Globals
-var restartButton = document.getElementById('restartButton');
-var spaces = document.getElementsByClassName('space');
-var symbols = ['O', 'X'];
-var turn = 0;
-var notification = document.getElementById('notification');
-var winnerMessage = document.getElementById('winnerMessage');
+var tictactoe = new Vue({
 
-// Waits for page to load before doing interesting things
-document.onreadystatechange = function() {
-  if (document.readyState == "interactive") {
-    restartButton.onclick = startGame;
-    startGame();
-  }
-};
+	el: '#tictactoe',
 
-function startGame() {
+	data: {
+		
 
-  // Reset turn counter
-  turn = 0;
+		turn: 0,
+		currentPlayer: "",
+		symbols : ["X","O"]
 
-  // Remove winner notification and make sure div is hidden
-  winnerMessage.innerHTML = '';
-  notification.style.display = 'none';
 
-  // Clear board and add click events on squares
-  for (var i = 0; i < spaces.length; i++) {
-    spaces[i].innerHTML = '';
-    spaces[i].addEventListener("click", takeSpace);
-  }  
+	},
 
-}
+	methods: {
+		startGame: function(){
 
-function takeSpace() {
+			alert("startGame");
+		},
 
-  turn++;
-  var currentPlayer = symbols[turn % 2];
-  this.innerHTML = currentPlayer;
-  this.removeEventListener("click", takeSpace);  
+		checkForWin: function(winArray){
+			// return this.spaces[winArray[0]] !== '' && 
+	  //   this.spaces[winArray[0]] === this.spaces[winArray[1]] && 
+	  //   this.spaces[winArray[0]]=== this.spaces[winArray[2]];
 
-  for (var i = 0; i < wins.length; i++) {
-    if (checkForWin(wins[i])) {
+		}
 
-      // No more clicking!
-      for (var j = 0; j < spaces.length; j++) {
-        spaces[j].removeEventListener("click", takeSpace);
-      }
-      // Notify the players
-      notification.style.display = 'block';
-      winnerMessage.innerHTML = "Yay! " + currentPlayer + " won!";
-    }
-    else {
-      if (turn == 9) {
-        if(notification.style.display != "block"){
-        notification.style.display = "block";
-        winnerMessage.innerHTML = "Bummer! It's a draw!";
-        }
-      }
-    }
-  }
+	},
+	computed: {
 
-}
 
-function checkForWin(winArray) {
-    
-  return spaces[winArray[0]].innerHTML !== '' && 
-    spaces[winArray[0]].innerHTML === spaces[winArray[1]].innerHTML && 
-    spaces[winArray[0]].innerHTML === spaces[winArray[2]].innerHTML;
-  
-}
 
+		takeSpace: function(){
+		
+			this.turn++;
+			this.currentPlayer=this.symbols[this.turn%2];
+			console.log(this.currentPlayer);
+			this.innerHTML=this.currentPlayer;
+
+			//checkForWin;
+		}
+
+	}
+
+});
