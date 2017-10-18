f1e68f3c6ebea861f87f4d83f2d7fd458db09bf0
Janine Hempy <jhempy@gmail.com>
2017-10-18 12:01:14 -0400

    Yay, it works

--- app/main.js
+++ app/main.js
@@ -5,13 +5,13 @@ Vue.component('space', {
       display: '',
     };
   },
-  props: [ 'symbol' ],
+  props: [ 'symbol', 'location' ],
   methods: {
     callSquare: function() {
       if (this.display === '') {
         // New square
         this.display = this.symbol;
-        this.$emit('new');
+        this.$emit('new', this.location);
       }
       else {
         // I have been clicked already
@@ -26,27 +26,85 @@ var app = new Vue({
   el: '#app',
   data: {
     symbols: ['X', 'O'],
-    turn: 0
+    turn: 0,
+    marks: {
+      X: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      O: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    },
+    winnerMessage: '',
+    showNotification: false
   },
   methods: {
 
-    newClick: function() {
-      console.log("clicked a new space");
+    newClick: function(location) {
+      
+      // Mark location array here! Before incrementing turn!
+      this.marks[this.symbols[this.turn%2]][location] = 1;
+
       this.turn++;
-      this.checkForWin();
-      this.checkForDraw();
+
+      // Only need to check for win condition if turn > 4
+      if (this.turn > 4) {
+        this.isGameOver();
+      }
+
+      // At this point, board is full if turn == 9
+      if (this.turn === 9) {
+        this.checkForDraw();
+      }
     },
 
     repeatClick: function() {
-      console.log("clicked a space again");
+      // Chill....
     },
 
-    checkForWin: function() {
-      alert("checking for win");
+    isGameOver: function() {
+
+      var self = this;
+
+      for (var i=0; i<wins.length; i++) {
+        Object.keys(this.marks).forEach(function(symbol) {
+          
+          var gameWon = self.checkForWin(wins[i], self.marks[symbol]);
+
+          console.log(gameWon);
+
+          if (gameWon) {
+            self.showNotification = true;
+            self.winnerMessage = symbol + " won the game!";
+          }
+
+        });
+      }
+
+    },
+
+    checkForWin: function(line, testArray) {
+      return testArray[line[0]] * testArray[line[1]] * testArray[line[2]];
     },
 
     checkForDraw: function() {
-      alert("checking for draw");
+    },
+
+    resetGame: function() {
+
+      // This reloads the whole page! Dangit!
+      location.reload();
+
+      // A better Vue solution would be to modify the components
+      // How can we do that?
+      // Maybe rewrite them to be bound to variables in app rather than
+      // receive their symbol via prop.
+      // Then we'd also need to do:
+            // this.turn = 0;
+            // this.marks = {
+            //   X: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            //   O: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+            // };
+            // this.winnerMessage = '';
+            // this.showNotification = false;
+      // to fully reset app state.
+
     }
 
 
